bc59041d7e8c3d23b0f4696aea0e10b4808ffe72
unknown <Jackson.Lango@bsg-27.bus.oregonstate.edu>
2015-07-21 14:52:40 -0700

    add art files

--- art/art-data.js
+++ art/art-data.js
@@ -1,5 +1,15 @@
 var imageFileNames = [
   'rubicon_demo.png',
   'spiderqueen_demo.gif',
-  'town_portfolio.png'
+  'town_portfolio.png',
+  'constructplus.gif',
+  'plateau.png',
+  'bow.gif',
+  'construct.gif',
+  'spiderlair.png',
+  'worm.gif',
+  'displacer.gif',
+  'park2.jpg',
+  'vintaje.png',
+  'canvasser.png'
 ]
